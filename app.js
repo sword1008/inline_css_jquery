565ae78b9ca4993ada617610b69c8e7f3e7838ce
Mihail <mihail2611008@gmail.com>
2015-10-30 05:05:48 +0200

    Fix post function.

--- app.js
+++ app.js
@@ -23,8 +23,10 @@ app.get('/jquery.inlineStyler.min.js', function(req, res){
 
 app.post('/postContent', function(req, res){
     var content= req.body.after;
+    var newone = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script> <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"> </script><link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
+    var result = content + newone;
     console.log('content:' + req.body.after);
-    res.send(content);
+    res.send(result);
 });
 
 var server = app.listen(3000, function () {
